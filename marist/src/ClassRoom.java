4bae902c258a9a8adacf4b211deb8f66a30aeb4b
vaidehics699 <115362801+vaidehics699@users.noreply.github.com>
2022-10-11 08:53:51 +0530

    Update ClassRoom.java

--- marist/src/ClassRoom.java
+++ marist/src/ClassRoom.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class ClassRoom extends Room {
@@ -8,6 +9,13 @@ public class ClassRoom extends Room {
         this.roomId = roomId;
         this.capacity = capacity;
         this.course = course;
+    }
+
+    public ClassRoom(int roomId, int capacity, List<Course> course, int doors){
+        this.roomId = roomId;
+        this.capacity = capacity;
+        this.course = course;
+        this.noOfDoors = doors;
     }
 
     @Override
@@ -26,6 +34,42 @@ public class ClassRoom extends Room {
     public void setCourse(List<Course> course) {
         this.course = course;
     }
+
+
+    public Course searchCourse(String name){
+        for(Course course1 : course){
+            if(course1.getCourseName().equals(name)){
+                return course1;
+            }
+        }
+        return null;
+    }
+
+
+    public int totalNumberOfCourses(){
+        return course.size();
+    }
+
+    public List<Course> getAllComputerScienceCourses(){
+        List<Course> courses = new ArrayList<>();
+        for(Course course1 : course){
+            if(course1.getCourseName().contains("CS")){
+                courses.add(course1);
+            }
+        }
+        return courses;
+    }
+
+    public void removeCourse(int courseId){
+        List<Course> courseList = new ArrayList<>();
+        for(Course course1 : course){
+            if(course1.getCourseId() != courseId){
+               courseList.add(course1);
+            }
+        }
+        this.course = courseList;
+    }
+
 
     public ClassRoom() {
     }
